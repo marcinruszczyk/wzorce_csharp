4069f8242edcafb7616c3d430f8817fe7bbd72eb
Marcin <marcin.ruszczyk@wp.pl>
2018-07-08 17:01:55 +0200

    Adapter added

---
+++ Structural Patterns/adapter/Adapter/Adapter/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+namespace Adapter
+{
+    class Program
+
+    {
+    
+        static void Main()
+        {
+            // Create adapter and place a request
+
+            Target target = new Adapter();
+            target.Request();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Target
+
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+
+    
+
+    class Adapter : Target
+
+    {
+        private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            // Possibly do some other work
+
+            //  and then call SpecificRequest
+
+            _adaptee.SpecificRequest();
+        }
+    }
+        
+
+    class Adaptee
+
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+}
