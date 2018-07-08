69e8a6b4013f6cc150b6411aec0f5af00aed2781
Marcin <marcin.ruszczyk@wp.pl>
2018-07-08 13:57:49 +0200

    factory method added

---
+++ Creational Patterns/factory_method/Factory_method/Factory_method/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Factory_method
+{
+   
+    class Program
+
+    {
+       
+        static void Main()
+        {
+            // An array of creators
+
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            // Iterate over creators and create products
+
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}",
+                  product.GetType().Name);
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    
+    abstract class Product
+
+    {
+    }
+
+    
+    class ConcreteProductA : Product
+
+    {
+    }
+
+    
+    class ConcreteProductB : Product
+
+    {
+    }
+
+    
+    abstract class Creator
+
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    
+    class ConcreteCreatorA : Creator
+
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+   
+    class ConcreteCreatorB : Creator
+
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
