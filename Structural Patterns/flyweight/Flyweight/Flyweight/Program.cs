a65ff5e20712e4df41eca4407ce719f4daec81bc
Marcin <marcin.ruszczyk@wp.pl>
2018-07-09 18:47:59 +0200

    Flyweight simple example

---
+++ Structural Patterns/flyweight/Flyweight/Flyweight/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+ namespace Flyweight
+{
+   
+
+    class Program
+
+
+    {
+
+        static void Main()
+        {
+            // Arbitrary extrinsic state
+
+            int extrinsicstate = 22;
+
+            FlyweightFactory factory = new FlyweightFactory();
+
+            // Work with different flyweight instances
+
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight fu = new
+
+              UnsharedConcreteFlyweight();
+
+            fu.Operation(--extrinsicstate);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+   
+
+    class FlyweightFactory
+
+    {
+        private Hashtable flyweights = new Hashtable();
+
+        // Constructor
+
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            return ((Flyweight)flyweights[key]);
+        }
+    }
+
+    
+    abstract class Flyweight
+
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+
+    
+
+    class ConcreteFlyweight : Flyweight
+
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+
+    
+    class UnsharedConcreteFlyweight : Flyweight
+
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("UnsharedConcreteFlyweight: " +
+              extrinsicstate);
+        }
+    }
+}
+
