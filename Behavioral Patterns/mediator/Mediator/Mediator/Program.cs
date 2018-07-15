e1dcf2ee8cd92797efe078a90d0c24ef6c3e71af
Marcin <marcin.ruszczyk@wp.pl>
2018-07-15 15:54:24 +0200

    Mediator example

---
+++ Behavioral Patterns/mediator/Mediator/Mediator/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mediator
+{
+    class Program
+
+    {
+          static void Main()
+        {
+            ConcreteMediator m = new ConcreteMediator();
+
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    
+
+    abstract class Mediator
+
+    {
+        public abstract void Send(string message,
+          Colleague colleague);
+    }
+
+    
+    class ConcreteMediator : Mediator
+
+    {
+        private ConcreteColleague1 _colleague1;
+        private ConcreteColleague2 _colleague2;
+
+        public ConcreteColleague1 Colleague1
+        {
+            set { _colleague1 = value; }
+        }
+
+        public ConcreteColleague2 Colleague2
+        {
+            set { _colleague2 = value; }
+        }
+
+        public override void Send(string message,
+          Colleague colleague)
+        {
+            if (colleague == _colleague1)
+            {
+                _colleague2.Notify(message);
+            }
+            else
+
+            {
+                _colleague1.Notify(message);
+            }
+        }
+    }
+
+    
+    abstract class Colleague
+
+    {
+        protected Mediator mediator;
+
+        // Constructor
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+
+    
+    class ConcreteColleague1 : Colleague
+
+    {
+        // Constructor
+
+        public ConcreteColleague1(Mediator mediator)
+          : base(mediator)
+        {
+        }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        {
+            Console.WriteLine("Colleague1 gets message: "
+
+              + message);
+        }
+    }
+
+   
+    class ConcreteColleague2 : Colleague
+
+    {
+        // Constructor
+
+        public ConcreteColleague2(Mediator mediator)
+          : base(mediator)
+        {
+        }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        {
+            Console.WriteLine("Colleague2 gets message: "
+
+              + message);
+        }
+    }
+}
